68c64d3f15cbaf2e7dd5fe7f1e73b9f5f6c25ed2
Emotion <111930499+Emotion06@users.noreply.github.com>
2023-04-18 19:23:42 +0200

    Add files via upload

---
+++ languages/de.lua
@@ -0,0 +1,30 @@
+-------------------------------------------------------------------------
+-----------------------------   German    -------------------------------
+-------------------------------------------------------------------------
+
+Locales["de"] = {
+
+    -- Prompts
+    traveltoguarmaprice = "Preis: "..Config.PriceToGuarma.."$",
+    traveltomainlandprice = "Preis: "..Config.PriceForReturn.."$",
+    traveltoguarma = "Nach Guarma",
+    traveltomainland = "Zurück ans Festland",
+
+    -- Notifcations
+    travelto = "Du fährst nach Guarma für "..Config.PriceToGuarma.."$",
+    travelfrom = "Du fährst zurück ans Festland für "..Config.PriceToGuarma.."$",
+    --nomoney = "Du hast nicht genug Geld",
+
+    -- Transitions
+    toguarmatitle = "Guarma",
+    toguarmasubtitle = "El Bahia Del Paz",
+    toguarmamessage = "Das Boot legt ab. .",
+
+    tomainlandtitle = "Festland",
+    tomainlandsubtitle = "Saint Denis Harbor",
+    tomainlandmessage = "Züruck nach Hause!. .",
+}
+
+-------------------------------------------------------------------------
+-------------------------------   End    --------------------------------
+-------------------------------------------------------------------------
---
+++ languages/en.lua
@@ -0,0 +1,30 @@
+-------------------------------------------------------------------------
+-----------------------------   English    -------------------------------
+-------------------------------------------------------------------------
+
+Locales["en"] = {
+
+    -- Prompts
+    traveltoguarmaprice = "Price: "..Config.PriceToGuarma.."$",
+    traveltomainlandprice = "Price: "..Config.PriceForReturn.."$",
+    traveltoguarma = "To Guarma",
+    traveltomainland = "Back to mainland",
+
+    -- Notifcations
+    travelto = "You go to Guarma for "..Config.PriceToGuarma.."$",
+    travelfrom = "You go back to the mainland for "..Config.PriceToGuarma.."$",
+    --nomoney = "Du hast nicht genug Geld",
+
+    -- Transitions
+    toguarmatitle = "Guarma",
+    toguarmasubtitle = "El Bahia Del Paz",
+    toguarmamessage = "Your boat is sailing. .",
+
+    tomainlandtitle = "Festland",
+    tomainlandsubtitle = "Saint Denis Harbor",
+    tomainlandmessage = "Feeling refreshed. .",
+}
+
+-------------------------------------------------------------------------
+-------------------------------   End    --------------------------------
+-------------------------------------------------------------------------
